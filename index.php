68ac192453455f872c7c3c31386a2a3dc5dd3189
NikitaBenzin <nikitory05@gmail.com>
2024-03-15 17:36:06 +0100

    add header section

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bazy Danych - sklep</title>
-    <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bazy Danych - sklep</title>
+	<link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="./css/styles.css">
+	<link rel="stylesheet" href="./css/header.css">
 </head>
+
 <body>
 
-    <div class="container">
+	<div class="container">
+		<?PHP
+		include './components/header.php';
+		?>
 
 
+	</div>
 
-    </div>
-
-    <!-- SCRIPTS -->
-    <script src="./js/bootstrap/bootstrap.bundle.min.js"></script>
-    <script src="./js/jq.js"></script>
-    <script src="./js/app.js"></script>
+	<!-- SCRIPTS -->
+	<script src="./js/bootstrap/bootstrap.bundle.min.js"></script>
+	<script src="./js/jq.js"></script>
+	<script src="./js/app.js"></script>
 </body>
+
 </html>
